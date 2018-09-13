fcf3eecb4bccfb5a5f7362323eada398b7132df4
以对无言 <1176627709@qq.com>
2018-09-13 13:45:16 +0800

    完成key2filename与filenamekey
    通过测试，没有问题

--- Server/Upload/lib.py
+++ Server/Upload/lib.py
@@ -1,7 +1,16 @@
+import random
+import string
+from Request.models import Key
+
 def unique(fileName):  # fileName: 传入的文件名
     return fileName  # return 绝对唯一的文件名
 
 
-def fileName2key(fileName):  # fileName: 绝对唯一的文件名
-    key = fileName
-    return key  # 8位密钥，前四位为日期，后四位随机
+def fileName2key(fileName, password):
+    buf = string.digits
+    former = ''.join(random.choice(buf) for _ in range(4))
+    key = former + password
+    while Key.objects.filter(key=key, is_print=False).count():
+        former = ''.join(random.choice(buf) for _ in range(4))
+        key = former + password
+    return key
